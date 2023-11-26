d54c112280faa090e729c8d9ee3bb3351f9080bb
Theod0reWu <43049406+Theod0reWu@users.noreply.github.com>
2023-11-26 01:22:08 -0500

    file renaming

---
+++ python/datagen.py
@@ -0,0 +1,11 @@
+from simulation import CoarseDiffsion, BrownianMotion, ParticleDynamics
+from simdata import BoundaryCondition
+import numpy as np
+
+import os
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
